935512f64bcf3f2f9672b6aaa16ed93ef4cbe3d9
Documenter.jl <documenter@juliadocs.github.io>
2021-09-06 11:14:05 +0000

    build based on ba22f01

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = entsoe_api","category":"page"},{"location":"#entsoe_api","page":"Home","title":"entsoe_api","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for entsoe_api.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [entsoe_api]","category":"page"}]
+}
